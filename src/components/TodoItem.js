b9cccbd21756465d12507e56a004cf4ca822dfd7
Ilyas <ilyas.bakhytzhan@gmail.com>
2025-03-06 15:35:43 +0500

    third commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function TodoItem({ todo, toggleComplete, deleteTodo }) {
+  return (
+    <div className="todo-item">
+      <input 
+        type="checkbox" 
+        checked={todo.completed} 
+        onChange={() => toggleComplete(todo.id)}
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default TodoItem;
